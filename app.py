6e724549e6b21722f3629108457b4cb36d5f9486
DavidHoffman11 <dghoffman@smu.edu>
2022-05-05 12:24:17 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 #import required packages
+from zlib import DEF_BUF_SIZE
 from flask import Flask, render_template, request
 import jsonify
 import requests
 import pickle
 import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 # create a Flask object
 app = Flask("WorldCup_model")
 
 # load the ml model which we have saved earlier in .pkl format
-# model = pickle.load(open('world_cup_model.pkl', 'rb'))
+model = pickle.load(open('world_cup_model.pkl', 'rb'))
 
 # define the route(basically url) to which we need to send http request
 # HTTP GET request method
@@ -26,56 +26,32 @@ app = Flask("WorldCup_model")
 def Home():
     return render_template('index.html')
 
-# creating object for StandardScaler
-standard_to = StandardScaler()
-
 # define the predict function which is going to predict the results from ml model based on the given values through html form
 def predict():
 
+    # initialize dataframe
+    game = pd.DataFrame(columns=['Team', 'ATT', 'MID', 'DEF', 'OVR'])
+    i = 1
+
     if request.method == 'POST':
         # Use request.form to get the data from html form through post method.
-        Year = int(request.form['Year'])
-        Year = 2020 - Year
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        while (i < 33):
+            Team = request.form['Team'+i]
+            ATT = int(request.form['ATT'+i])
+            MID = int(request.form['MID'+i])
+            DEF = int(request.form['DEF'+i])
+            OVR = int(request.form['OVR'+i])
+            game = game.append({'Team': Team, 'ATT': ATT, 'MID': MID, 'DEF': DEF, 'OVR': OVR}, ignore_index=True)
         
-        #Fuel_Type(feature) is categorised into petrol, diesel, cng, therefore we have done one-hot encoding on it while building model 
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        elif(Fuel_Type_Petrol=='Diesel'):
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=0
-            
-        #Seller_type(feature) is categorised into indivividual and dealer,therefore we have done one-hot encoding on it while building model
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
-        else:
-            Seller_Type_Individual=0
-            
-        #Transmission mannual(feature) is categorised into mannual and automatic,therefore we have done one-hot encoding on it while building model
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
+        prediction=model.predict([[game]])
         
         #condition for invalid values
-        if output<0:
-            return render_template('index.html',prediction_text="Sorry you cannot sell this car")
+        if prediction<0:
+            return render_template('index.html',prediction_text="Unable to compute result")
         
         #condition for prediction when values are valid
         else:
-            return render_template('index.html',prediction_text="You Can Sell the Car at {} lakhs".format(output))
+            return render_template('index.html',prediction_text="Using given data, {} will win the World Cup".format(prediction))
         
     #html form to be displayed on screen when no values are inserted; without any output or prediction
     else:
